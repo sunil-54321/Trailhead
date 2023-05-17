4cfb78cabb634f9f03aecb44ce4f003af0216cc0
sunil-54321 <you@email.com>
2023-05-18 00:26:01 +0530

    button modification

--- force-app/main/default/lwc/showToastmessage/showToastmessage.js
+++ force-app/main/default/lwc/showToastmessage/showToastmessage.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import{ShowToastEvent} from 'lightning/platformShowToastEvent';
+export default class ShowToastmessage extends LightningElement {
+    myTitle= 'Gyan Bhandar';
 
-export default class ShowToastmessage extends LightningElement {}
+    //1st function
+    handleclick(){
+        this.showToast();
+    }
+
+    showToast(){
+        const event =new ShowToastEvent({
+            title:'show Toast Demo',
+            message:'Do you love me to click if, click on my child buttton'
+        });
+        this.dispatchEvent (event);
+    }
+}
